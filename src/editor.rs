edca9b032601cafe8f87e5e764e8b9c8249eb53e
Alex Yu <alexy23@g.ucla.edu>
2022-05-14 14:04:14 -0700

    Add scroll support

--- src/editor.rs
+++ src/editor.rs
@@ -22,6 +22,7 @@ pub struct Editor {
     should_quit: bool,
     terminal: Terminal,
     cursor_position: Position,
+    offset: Position,
     document: Document,
 }
 
@@ -39,6 +40,7 @@ impl Editor {
             should_quit: false,
             terminal: Terminal::default().expect("Failed to initialize terminal"),
             document,
+            offset: Position::default(),
             cursor_position: Position::default(),
         }
     }
@@ -67,15 +69,19 @@ impl Editor {
             println!("Goodbye.\r");
         } else {
             self.draw_rows();
-            Terminal::cursor_position(&self.cursor_position);
+            Terminal::cursor_position(&Position {
+                x: self.cursor_position.x.saturating_sub(self.offset.x),
+                y: self.cursor_position.y.saturating_sub(self.offset.y),
+            });
         }
         Terminal::cursor_show();
         Terminal::flush()
     }
 
     pub fn draw_row(&self, row: &Row) {
-        let start = 0;
-        let end = self.terminal.size().width as usize;
+        let width = self.terminal.size().width as usize;
+        let start = self.offset.x;
+        let end = self.offset.x + width;
         let row = row.render(start, end);
         println!("{}\r", row);
     }
@@ -84,7 +90,7 @@ impl Editor {
         let height = self.terminal.size().height;
         for terminal_row in 0..height-1 {
             Terminal::clear_current_line();
-            if let Some(row) = self.document.row(terminal_row as usize) {
+            if let Some(row) = self.document.row(terminal_row as usize + self.offset.y) {
                 self.draw_row(row);
             } else if self.document.is_empty() && terminal_row == height / 3 {
                 self.draw_welcome_message();
@@ -118,14 +124,39 @@ impl Editor {
             | Key::Home => self.move_cursor(pressed_key),
             _ => (),
         }
+        self.scroll();
         Ok(())
     }
 
+    fn scroll(&mut self) {
+        let Position { x, y } = self.cursor_position;
+        let height = self.terminal.size().height as usize;
+        let width = self.terminal.size().width as usize;
+        let mut offset = &mut self.offset;
+        
+        if y < offset.y {
+            offset.y = y;
+        } else if y >= offset.y.saturating_add(height) {
+            offset.y = y.saturating_sub(height).saturating_add(1);
+        }
+
+        if x < offset.x {
+            offset.x = x;
+        } else if x >= offset.x.saturating_add(width) {
+            offset.x = x.saturating_sub(width).saturating_add(1);
+        }
+    }
+
     fn move_cursor(&mut self, key: Key) {
+        let terminal_height = self.terminal.size().height as usize;
         let Position { mut x, mut y } = self.cursor_position;
-        let size = self.terminal.size();
-        let height = size.height.saturating_sub(1) as usize;
-        let width = size.width.saturating_sub(1) as usize;
+        let height = self.document.len();
+        let mut width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
+
         match key {
             Key::Up        => y = y.saturating_sub(1),
             Key::Down      => {
@@ -133,18 +164,40 @@ impl Editor {
                     y = y.saturating_add(1);
                 }
             }
-            Key::Left      => x = x.saturating_sub(1),
-            Key::Right     => {
-                if x < width {
-                    x = x.saturating_add(1);
+            Key::Left      => {
+                if x > 0 {
+                    x -= 1;
+                } else if y > 0 {
+                    y -= 1;
+                    if let Some(row) = self.document.row(y) {
+                        x = row.len();
+                    } else {
+                        x = 0;
+                    }
                 }
             },
-            Key::Ctrl('f') => y = 0,
-            Key::Ctrl('b') => y = height,
+            Key::Right     => {
+                if x < width {
+                    x += 1;
+                } else if y < height {
+                    y += 1;
+                    x = 0;
+                }
+            },
+            Key::Ctrl('b') => y = y.saturating_sub(terminal_height),
+            Key::Ctrl('f') => y = y.saturating_add(terminal_height).min(height),
             Key::Home      => x = 0,
             Key::End       => x = width,
             _              => (),
         }
+
+        width = if let Some(row) = self.document.row(y) {
+            row.len()
+        } else {
+            0
+        };
+        x = x.min(width);
+
         self.cursor_position = Position { x, y };
     }
 }
