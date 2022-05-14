9e32aeebf2b06bbb1944cb6b9ca3792e9a2fc53d
Alex Yu <alexy23@g.ucla.edu>
2022-05-13 23:22:36 -0700

    Change PageUp/PageDown key binding

--- src/editor.rs
+++ src/editor.rs
@@ -89,8 +89,7 @@ impl Editor {
             | Key::Down
             | Key::Left
             | Key::Right
-            | Key::PageUp
-            | Key::PageDown
+            | Key::Ctrl('f' | 'b')
             | Key::End
             | Key::Home => self.move_cursor(pressed_key),
             _ => (),
@@ -104,23 +103,23 @@ impl Editor {
         let height = size.height.saturating_sub(1) as usize;
         let width = size.width.saturating_sub(1) as usize;
         match key {
-            Key::Up       => y = y.saturating_sub(1),
-            Key::Down     => {
+            Key::Up        => y = y.saturating_sub(1),
+            Key::Down      => {
                 if y < height {
                     y = y.saturating_add(1);
                 }
             }
-            Key::Left     => x = x.saturating_sub(1),
-            Key::Right    => {
+            Key::Left      => x = x.saturating_sub(1),
+            Key::Right     => {
                 if x < width {
                     x = x.saturating_add(1);
                 }
             },
-            Key::PageUp   => y = 0,
-            Key::PageDown => y = height,
-            Key::Home     => x = 0,
-            Key::End      => x = width,
-            _             => (),
+            Key::Ctrl('f') => y = 0,
+            Key::Ctrl('b') => y = height,
+            Key::Home      => x = 0,
+            Key::End       => x = width,
+            _              => (),
         }
         self.cursor_position = Position { x, y };
     }
