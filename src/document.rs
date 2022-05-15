a1e3278bdb88fb6f82fbe8e0e37f393c142cf1e5
Alex Yu <alexy23@g.ucla.edu>
2022-05-15 10:45:47 -0700

    Handle character insertion and deletion

--- src/document.rs
+++ src/document.rs
@@ -1,5 +1,7 @@
-use std::fs;
+use crate::Position;
 use crate::Row;
+use std::cmp::Ordering;
+use std::fs;
 
 #[derive(Default)]
 pub struct Document {
@@ -26,6 +28,50 @@ impl Document {
             rows,
             filename: Some(filename.to_string()),
         })
+    }
+
+    fn insert_newline(&mut self, at: &Position) {
+        let len = self.len();
+        if at.y <= len {
+            if at.y == len {
+                self.rows.push(Row::default());
+            } else {
+                let new_row = self.rows.get_mut(at.y).unwrap().split(at.x);
+                self.rows.insert(at.y + 1, new_row);
+            }
+        }
+    }
+    
+    pub fn insert(&mut self, at: &Position, c: char) {
+        if c == '\n' {
+            self.insert_newline(at);
+            return;
+        }
+        
+        match at.y.cmp(&self.len()) {
+            Ordering::Equal => {
+                let mut row = Row::default();
+                row.insert(0, c);
+                self.rows.push(row);
+            },
+            Ordering::Less => {
+                let row = self.rows.get_mut(at.y).unwrap();
+                row.insert(at.x, c);
+            },
+            _ => (),
+        }
+    }
+    
+    pub fn delete(&mut self, at: &Position) {
+        let len = self.len();
+        if at.x == self.rows.get_mut(at.y).unwrap().len() && at.y < len - 1 {
+            let next_row = self.rows.remove(at.y + 1);
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.append(&next_row);
+        } else {
+            let row = self.rows.get_mut(at.y).unwrap();
+            row.delete(at.x);
+        }
     }
 
     pub fn row(&self, index: usize) -> Option<&Row> {
--- src/row.rs
+++ src/row.rs
@@ -27,6 +27,44 @@ impl Row {
         result
     }
 
+    pub fn insert(&mut self, at: usize, c: char) {
+        if at >= self.len() {
+            self.string.push(c);
+        } else {
+            let mut result: String = self.string[..].graphemes(true).take(at).collect();
+            let remainder: String = self.string[..].graphemes(true).skip(at).collect();
+
+            result.push(c);
+            result.push_str(&remainder);
+            self.string = result;
+        }
+
+        self.update_len();
+    }
+
+    pub fn append(&mut self, other: &Self) {
+        self.string = format!("{}{}", self.string, other.string);
+        self.update_len();
+    }
+
+    pub fn delete(&mut self, at: usize) {
+        if at < self.len() {
+            let mut result: String = self.string[..].graphemes(true).take(at).collect();
+            let remainder: String = self.string[..].graphemes(true).skip(at+1).collect();
+            result.push_str(&remainder);
+            self.string = result;
+            self.update_len();
+        }
+    }
+
+    pub fn split(&mut self, at: usize) -> Self {
+        let beginning: String = self.string[..].graphemes(true).take(at).collect();
+        let remainder: String = self.string[..].graphemes(true).skip(at).collect();
+        self.string = beginning;
+        self.update_len();
+        Self::from(&remainder[..])
+    }
+
     pub fn len(&self) -> usize {
         self.len
     }
