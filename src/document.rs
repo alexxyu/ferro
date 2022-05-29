2eff086e293f378658296ec99d6661bd31f0bae2
Alex Yu <alexy23@g.ucla.edu>
2022-05-28 18:30:39 -0700

    highlighting: Update multiline comment highlighting more often

--- src/document.rs
+++ src/document.rs
@@ -29,12 +29,17 @@ impl Document {
     /// specified by `filename`
     pub fn open(filename: &str) -> Result<Self, std::io::Error> {
         let contents = fs::read_to_string(filename)?;
+        let mut start_with_comment = false;
         let mut rows = Vec::new();
         let file_type = FileType::from(filename);
 
         for line in contents.lines() {
             let mut row = Row::from(line);
-            row.highlight(file_type.highlighting_options(), None, false);
+            start_with_comment = row.highlight(
+                file_type.highlighting_options(),
+                None,
+                start_with_comment
+            );
             rows.push(row);
         };
 
@@ -55,9 +60,7 @@ impl Document {
             self.rows.push(Row::default());
         } else {
             let current_row = &mut self.rows[at.y];
-            let mut new_row = current_row.split(at.x);
-            current_row.highlight(self.file_type.highlighting_options(), None, false);
-            new_row.highlight(self.file_type.highlighting_options(), None, false);
+            let new_row = current_row.split(at.x);
             self.rows.insert(at.y + 1, new_row);
         }
     }
@@ -70,19 +73,16 @@ impl Document {
         self.dirty = true;
         if c == '\n' {
             self.insert_newline(at);
-            return;
-        }
-        
-        if at.y == self.rows.len() {
+        } else if at.y == self.rows.len() {
             let mut row = Row::default();
             row.insert(0, c);
-            row.highlight(self.file_type.highlighting_options(), None, false);
             self.rows.push(row);
         } else {
             let row = &mut self.rows[at.y];
             row.insert(at.x, c);
-            row.highlight(self.file_type.highlighting_options(), None, false);
         }
+
+        self.highlight(None);
     }
     
     pub fn delete(&mut self, at: &Position) {
@@ -96,12 +96,12 @@ impl Document {
             let next_row = self.rows.remove(at.y + 1);
             let row = &mut self.rows[at.y];
             row.append(&next_row);
-            row.highlight(self.file_type.highlighting_options(), None, false);
         } else {
             let row = &mut self.rows[at.y];
             row.delete(at.x);
-            row.highlight(self.file_type.highlighting_options(), None, false);
         }
+
+        self.highlight(None);
     }
 
     pub fn save(&mut self) -> Result<(), Error> {
