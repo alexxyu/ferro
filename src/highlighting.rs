753b29c711282712aba283726c25f2e61131cf8e
Alex Yu <alexy23@g.ucla.edu>
2022-06-06 15:52:24 -0700

    highlighting: Use black text if light terminal theme is detected

--- src/highlighting.rs
+++ src/highlighting.rs
@@ -1,4 +1,16 @@
+use lazy_static::lazy_static;
+use termbg::{self, Theme};
 use termion::color;
+
+lazy_static! {
+    static ref SHOULD_USE_DARK_THEME: bool = termbg::theme(std::time::Duration::from_millis(100))
+        .map_or(true, |res| {
+            match res {
+                Theme::Dark => true,
+                _ => false,
+            }
+        });
+}
 
 #[derive(PartialEq, Clone, Copy)]
 pub enum Type {
@@ -29,7 +41,7 @@ impl Type {
             Type::Comment | Type::MultilineComment => color::AnsiValue::rgb(3, 3, 3),
             Type::PrimaryKeywords => color::AnsiValue::rgb(0, 4, 5),
             Type::SecondaryKeywords => color::AnsiValue::rgb(0, 5, 4),
-            _ => color::AnsiValue::rgb(5, 5, 5),
+            _ => if *SHOULD_USE_DARK_THEME { color::AnsiValue::rgb(5, 5, 5) } else { color::AnsiValue::rgb(0, 0, 0) },
         }
     }
 }
